18745478911ac0d6ac268bf2f88c75f6db688f43
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-11-25 13:53:58 +0000

    とちう

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,21 @@
-const maximize_button=document.createElement("button");
-maximize_button.id="maximize_button";
-maximize_button.innerHTML=`
-<h1>MAXIMIZER!</h1>
-`;
+const maximize_button = document.createElement("button");
+maximize_button.id = "maximize_button";
+maximize_button.innerHTML = `<h1>MAXIMIZER!</h1>`;
+maximize_button.addEventListener("click", () => {
+  if (maximize_button.innerHTML == "<h1>MAXIMIZER!</h1>") {
+    document.body.requestFullscreen();
+    maximize_button.style.display = "none";
+    setTimeout(() => {
+      maximize_button.innerHTML = "<h1>?UNMAXIMIZE?</h1>";
+      maximize_button.style.display = "";
+    }, 5 * 1000);
+  } else {
+    try {
+      document.body.exitFullscreen();
+    } catch (error) {
+      console.info(error);
+    }
+    maximize_button.innerHTML = "<h1>MAXIMIZER!</h1>";
+  }
+});
+document.body.append(maximize_button);
