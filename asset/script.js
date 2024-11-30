295cb542793e24ff1005fdb96829bedbacf73516
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-11-30 04:38:58 +0000

    complete...?

--- asset/script.js
+++ asset/script.js
@@ -1,24 +1,30 @@
-let pixel_width, pixel_height, aspect_ratio;
-const maximize_button = document.createElement("button");
-maximize_button.id = "maximize_button";
-maximize_button.innerHTML = `<h1>START LOADING</h1>`;
-maximize_button.addEventListener("click", () => {
-  document.body
-    .requestFullscreen()
-    .then((maximize_button.style.display = "none"))
-    .then(() => {
-      pixel_width = innerWidth * devicePixelRatio;
-      pixel_height = innerHeight * devicePixelRatio;
-      aspect_ratio = pixel_width / pixel_height;
-      alert(
-        `Complete! Unmaximize Screen!\nScreen Size: ${pixel_width.toString()}, ${pixel_height.toString()}`
-      );
-    });
-});
-document.body.addEventListener("fullscreenchange", () => {
-  if (document.fullscreenElement == null) {
-    const content = document.querySelector("div.content");
-    content.style = `width:auto;height:90vh;aspect-ratio:${aspect_ratio.toString()};opacity:1;`;
-  }
-});
-document.body.append(maximize_button);
+let params = new URLSearchParams(document.location.search);
+if (params.has("embed")) {
+  const content = document.querySelector("div.content");
+  content.style = `width:100%;height:100%;opacity:1;`;
+} else {
+  let pixel_width, pixel_height, aspect_ratio;
+  const maximize_button = document.createElement("button");
+  maximize_button.id = "maximize_button";
+  maximize_button.innerHTML = `<h1>START LOADING</h1>`;
+  maximize_button.addEventListener("click", () => {
+    document.body
+      .requestFullscreen()
+      .then((maximize_button.style.display = "none"))
+      .then(() => {
+        pixel_width = innerWidth * devicePixelRatio;
+        pixel_height = innerHeight * devicePixelRatio;
+        aspect_ratio = pixel_width / pixel_height;
+        alert(
+          `Complete! Unmaximize Screen!\nScreen Size: ${pixel_width.toString()}, ${pixel_height.toString()}`
+        );
+      });
+  });
+  document.body.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement == null) {
+      const content = document.querySelector("div.content");
+      content.style = `width:auto;height:90vh;aspect-ratio:${aspect_ratio.toString()};opacity:1;`;
+    }
+  });
+  document.body.append(maximize_button);
+}
