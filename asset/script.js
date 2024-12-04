3cd4990ecdefa2db450d4af631dd5e74c0383a45
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-12-04 19:07:02 +0900

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,11 @@
 let params = new URLSearchParams(document.location.search);
 if (params.has("embed")) {
   const content = document.querySelector("div.content");
-  content.style = `width:100%;height:100%;opacity:1;`;
+  if (params.has("ratio")){
+    content.style = `width:auto;height:100%;aspect-ratio:${params.get("ratio")};opacity:1;`;
+  }else{
+    content.style = `width:100%;height:100%;opacity:1;`;
+  }
 } else {
   let pixel_width, pixel_height, aspect_ratio;
   const maximize_button = document.createElement("button");
