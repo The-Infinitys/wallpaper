30ed8566158ec5704edf57416604954ed930c931
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-12-05 10:47:56 +0900

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,4 @@
+let pixel_width, pixel_height, aspect_ratio;
 let params = new URLSearchParams(document.location.search);
 if (params.has("embed")) {
   const content = document.querySelector("div.content");
@@ -11,7 +12,6 @@ if (params.has("embed")) {
     content.style = `width:100%;height:100%;opacity:1;`;
   }
 } else {
-  let pixel_width, pixel_height, aspect_ratio;
   const maximize_button = document.createElement("button");
   maximize_button.id = "maximize_button";
   maximize_button.innerHTML = `<h1>START LOADING</h1>`;
