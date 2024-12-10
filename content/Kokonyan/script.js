d557b23477ab66eb4bd44cf133fbcc8f94e093e8
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-12-10 12:56:23 +0000

    あわあわ

--- content/Kokonyan/script.js
+++ content/Kokonyan/script.js
@@ -7,11 +7,28 @@ const create_bubble = () => {
   bubble_element.style.bottom = "0%";
   bubble_element.style.left = "0%";
   bg_elem.appendChild(bubble_element);
-  let count=0;
+  let count = 0;
+  let stroke_speed = 1 + 2 * Math.random();
+  let stroke_width = 1 + 2 * Math.random();
+  let stroke_left = 2 * Math.random() - 1;
+  let float_speed = 1 + 2 * Math.random();
   setInterval(() => {
     count++;
-    bubble_element.style.bottom = (count/10).toString() + "%";
-    bubble_element.style.left = (count).toString() + "%";
+    const bubble_height = 110 - (float_speed * count) / 10;
+    if (bubble_height <= -20) {
+      count = 0;
+      stroke_speed = 1 + 2 * Math.random();
+      stroke_width = 1 + 2 * Math.random();
+      stroke_left = 2 * Math.random() - 1;
+      float_speed = 1 + 2 * Math.random();
+    }
+    bubble_element.style.top = bubble_height.toString() + "%";
+    bubble_element.style.left =
+      (
+        80 +
+        10 * stroke_left +
+        2 * stroke_width * Math.sin((stroke_speed * count) / 50)
+      ).toString() + "%";
   }, 10);
 };
 for (let i = 0; i < 10; ++i) {
